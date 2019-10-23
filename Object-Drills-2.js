0d0cea666347698322168ac1dedc8a6c70d626bc
AnthonyTB <Anthonylol@protonmail.com>
2019-10-23 12:26:29 -0700

    FINALLY DONE OMG

--- Object-Drills-2.js
+++ Object-Drills-2.js
@@ -52,33 +52,76 @@ arr.forEach(function(element) {
   }
 });
 
-
-let decodeKey ={
-  a : 2,
-  b : 3,
-  c : 4,
-  d : 5
+let decodeKey = {
+  a: 2,
+  b: 3,
+  c: 4,
+  d: 5
 };
 
-function decoder(word){
+function decoder(word) {
   let position = -1;
-  for (let i in decodeKey){
-    if(word.charAt(0).toLowerCase() === i){
+  for (let i in decodeKey) {
+    if (word.charAt(0).toLowerCase() === i) {
       position = decodeKey[i];
     }
   }
-  if(position > 0){
+  if (position > 0) {
     return word.charAt(position - 1);
-  } 
+  }
   return ' ';
 }
 
-function decodeWords(string){
+function decodeWords(string) {
   let result = '';
   let array = string.split(' ');
-  array.forEach(function(element){
+  array.forEach(function(element) {
     let char = decoder(element);
     result += char;
   });
   return result;
+}
+
+let characters = [
+  createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6),
+  createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1),
+  createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2),
+  createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5),
+  createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8)
+];
+characters.push(createCharacter('Arwen Undomiel', 'Elfy', 'Half-Elf', 'Rivendell', 99, 99));
+let hobbits = characters.filter((i) => i.race === 'Hobbit');
+let opCharacters = characters.filter((i) => i.attack > 5);
+characters.find(function(i) {
+  if (i.nickName === 'aragorn') {
+    i.describe();
+  }
+});
+
+// console.log(hobbits);
+// console.log(opCharacters);
+
+function createCharacter(name, nickName, race, origin, attack, defense) {
+  return {
+    name,
+    nickName,
+    race,
+    origin,
+    attack,
+    defense,
+    describe: function() {
+      return `${this.name} is a ${this.race} from ${origin}`;
+    },
+    elvaluateFight: function(character) {
+      let x = this.attack - character.defense;
+      let y = character.attack - this.defense;
+      if (x < 0) {
+        x = 0;
+      }
+      if (y < 0) {
+        y = 0;
+      }
+      return `Your opponent takes ${x} damage and you recieve ${y} damage`;
+    }
+  };
 }
