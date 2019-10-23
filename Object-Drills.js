5c1b4abfc3e512b64f33d1ad1b231e445e1f2c30
AnthonyTB <Anthonylol@protonmail.com>
2019-10-23 09:07:47 -0700

    Step 1

--- Object-Drills.js
+++ Object-Drills.js
@@ -30,9 +30,22 @@ function personMaker() {
   return person;
 }
 
-
 function keyDeleter(obj) {
   delete obj.foo;
   delete obj.bar;
   return obj;
 }
+
+function makeStudentsReport(data) {
+  let newArr = [];
+  data.forEach(function(i) {
+    newArr.push(`${i.name}: ${i.grade}`);
+  });
+  return newArr;
+}
+
+const testData = [
+  { name: 'Jane Doe', grade: 'A' },
+  { name: 'John Dough', grade: 'B' },
+  { name: 'Jill Do', grade: 'A' }
+];
